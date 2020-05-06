ada39fe55dea6960328770047239cb19c9df121b
blyndusk <alexandre.delaloy.dev@gmail.com>
2020-05-07 00:08:14 +0200

    Update: rename ba.sh to woo.sh

--- ba.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-if [ $2 = "toc" ]; then
-  grep -E '\[[0-9]\.?[0-9]?\] - [A-Z]+' $1 | less
-elif [ $2 = "goto" ]; then
-  less +$(grep -nio "$3" $1 | head -n1 | cut -d : -f 1) $1
-fi
---
+++ woo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+if [[ $1 =~ [bB][aA][sS][hH] ]] ; then
+  FILE="bash-command-line.txt"
+fi
+
+if [[ $2 =~ [tT][oO][cC] ]]; then
+  grep -E '\[[0-9]\.?[0-9]?\] - [A-Z]+' $FILE | less
+elif [[ $2 =~ [gG][oO][tT][oO] ]]; then
+  less +$(grep -nio "$3" $FILE | head -n1 | cut -d : -f 1) $FILE
+fi
