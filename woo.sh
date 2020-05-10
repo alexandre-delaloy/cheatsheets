416babd5a9e6a9841c382a3ab3a7a4ba0a8af3ec
blyndusk <alexandre.delaloy.dev@gmail.com>
2020-05-10 22:39:57 +0200

    Fix: woosh script - add default

--- woo.sh
+++ woo.sh
@@ -9,4 +9,6 @@ if [[ $2 =~ [tT][oO][cC] ]]; then
   grep -E '\[[0-9]+(\.[0-9]+)?\] - [A-Z]+' $FILE | less
 elif [[ $2 =~ [gG][oO][tT][oO] ]]; then
   less +$(grep -nio "$3" $FILE | head -n1 | cut -d : -f 1) $FILE
+else
+  less $FILE
 fi
