66a9e0ee9c3d0ba9154c464cda4cbaf5b29cbcaf
blyndusk <alexandre.delaloy.dev@gmail.com>
2020-05-26 12:03:26 +0200

    Add: Docker chsh - run cmd

--- cheatsheets/docker-command-line.sh
+++ cheatsheets/docker-command-line.sh
@@ -9,62 +9,80 @@
   [CID]                                         Container ID                     
   [IID]                                         Image ID
   [IMAGE]                                       An image
+  [UPORT]                                       User port 
+  [CPORT]                                       Container port
+  [EDIR]                                        Exterior directory
+  [CDIR]                                        Container directory
 
 
-[1] - START A CONTAINER
+[1.1] - START A CONTAINER
 ----
   docker run [IMAGE]                            Run a container from [IMAGE]
   --                                            --
   docker run -d [IMAGE]                         Run the container as detached
+  docker run -p [UPORT]:[CPORT]                 Map [CPORT] port to [UPORT] port
+  docker run -v [EDIR]:[CDIR]                   Map [EDIR] volume to [CDIR] volume
+  docker run -i [IMAGE]                         Interactive mode
   docker run -it [IMAGE] [PROMPT]               Run the container interactively with given [PROMPT]
 E.G.
   docker run nginx
   docker run nginx:latest
-  docker run -d nginx
+  docker run -d -p 8080:8080 nginx
+  docker run -v /data/db:/var/lib/mysql
   docker run -it node bash
 
 
-[2] - STOP A CONTAINER
+[1.2] - STOP A CONTAINER
 ----
   docker stop [CID]                             Stop the container by [CID]
 E.G.
   docker stop 6ca2fb9d6969
 
 
-[3] - LIST CONTAINERS
+[1.3] - LIST CONTAINERS
 ----
   docker ps                                     List all running containers
 
 
-[4] - LIST IMAGES
+[1.4] - LIST IMAGES
 ----
   docker images                                 List all images 
 
 
-[5] - REMOVE A CONTAINER
+[1.5] - REMOVE A CONTAINER
 ----
   docker rm [CID]                               Remove a container by [CID]
 E.G.
   docker rmi 6ca2fb9d6969
 
 
-[6] - REMOVE AN IMAGE
+[1.6] - REMOVE AN IMAGE
 ----
   docker rmi [IID]                              Remove an iamge by [IID]
 E.G.
   docker rmi bdca973cfa07
 
 
-[7] - DOWNLOAD AN IMAGE
+[1.7] - DOWNLOAD AN IMAGE
 ----
   docker pull [IMAGE]                           Download [IMAGE]
 E.G.
   docker pull nginx:latest
 
 
-[8] - EXECUTE A COMMAND
+[1.8] - EXECUTE A COMMAND
 ----
-  docker exec [CID] [COMMAND]                   Execute [COMMAND] at container [CID]
+  docker exec [CID] [COMMAND]                   Execute [COMMAND] at the container using [CID]
+
+[1.9] - INSPECT A CONTAINER
+----
+  docker inspect [CID]                          Inspect the container using [CID]           
+
+
+[1.10] - LOGS INFORMATIONS ABOUT A CONTAINER
+----
+  docker logs [CID]                             Output the logs of the container using [CID]               
+
 
 
  ------------------------------------------------------------------------------------------------
